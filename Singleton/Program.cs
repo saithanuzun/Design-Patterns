13307e60f4938e95233d88170236330b9591b2c0
sait han uzun <saithan.uzun@gmail.com>
2023-03-16 13:47:07 +0000

    Singleton Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Singleton
+{
+    private static Singleton instance = null; 
+    public static Singleton Instance => instance ?? (instance = new Singleton());
+    public string Car { get; set; }
+    private Singleton()
+    {
+        
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //var mysingleton = new Singleton(); //private Constructor
+        var mySingleton = Singleton.Instance;
+        mySingleton.Car = "BMW";
+        var mySingleton2 = Singleton.Instance;
+        Console.WriteLine(mySingleton2.Car);//output: BMW
+        
+        
+    }
+}
